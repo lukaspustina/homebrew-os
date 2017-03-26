0f05c4aa66de550893e45fa483d053418a5ba79d
Lukas Pustina <lukas@pustina.net>
2017-03-26 12:56:20 +0200

    Republish rpdate to rat to 0.4.0

--- Formula/rat.rb
+++ Formula/rat.rb
@@ -2,7 +2,7 @@ class Rat < Formula
   desc "REST API tool - qeuery various REST APIs comfortabl"
   homepage "https://lukaspustina.github.io/rat/"
   url "https://github.com/lukaspustina/rat/archive/v0.4.0.tar.gz"
-  sha256 "bc09faff08fd0d05442b36ec25d6f32af729e779c93973cfd3a8eced02207db0"
+  sha256 "f381185d08536db2d7f11d15e000f3ee17f784c452ac6a049e6f54cf464d8fdf"
   head "https://github.com/lukaspustina/rat.git"
 
   depends_on "rust" => :build
