7a29620e9372ae3e11af023e6da93560c06021c8
Lukas Pustina <lukas@pustina.net>
2017-09-28 13:34:45 +0200

    Gnarz, checksums, granz

--- Formula/mhost.rb
+++ Formula/mhost.rb
@@ -2,7 +2,7 @@ class Mhost < Formula
   desc "Like `host`, but uses multiple DNS servers massively parallel and compares results"
   homepage "https://lukaspustina.github.io/mhost/"
   url "https://github.com/lukaspustina/mhost/archive/v0.0.1.tar.gz"
-  sha256 "7741c6aece68d40973106fb2e682d924683cdc167132145920cf524ae422e12f"
+  sha256 "06a43c0c1439637a410c146d6f6b0e4a656c205ac0c80bef04245262d9ab5e6a"
   head "https://github.com/lukaspustina/mhost.git"
 
   depends_on "rust" => :build
