6578fbb90253f5792b79604359ff368768c5727d
Lukas Pustina <lukas@pustina.net>
2017-11-04 12:24:15 +0100

    Bump rat to version v0.4.8

--- Formula/rat.rb
+++ Formula/rat.rb
@@ -1,8 +1,8 @@
 class Rat < Formula
   desc "REST API tool - qeuery various REST APIs comfortabl"
   homepage "https://github.com/lukaspustina/luftpost"
-  url "https://github.com/lukaspustina/rat/archive/v0.4.7.tar.gz"
-  sha256 "d15ffd1897c58ee6650a2f88e3fe238b52318d7b4a4e9b00be43e91ed4fc11f5"
+  url "https://github.com/lukaspustina/rat/archive/v0.4.8.tar.gz"
+  sha256 "90e33eb377a52aa144b458ae08523dfb3d11224b0388177f220cd51c045361fb"
   head "https://github.com/lukaspustina/rat.git"
 
   depends_on "rust" => :build
